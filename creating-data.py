96748c0f470b3b78201b204e8e526783005a3d97
moises-santos <83379577+moises-santos@users.noreply.github.com>
2021-10-04 16:51:51 -0300

    att criacao de dados

--- creating-data.py
+++ creating-data.py
@@ -17,6 +17,7 @@ import matplotlib.dates as mdates
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from windrose import WindroseAxes
 import random
+import scipy.stats as stats
 
 lengthData = 4465 # tamanho do dado a ser criado: equivale a 30 dias de 10 em 10 min
 quantSpike = 150 # quantidades de spikes
@@ -27,7 +28,7 @@ indexSpike = [random.randint(0, lengthData) for _ in range(quantSpike)]
 time = datetime.strptime('1/1/2000 00:00:00','%m/%d/%Y %H:%M:%S')
 dateTime = [time + _*timedelta(minutes = 10) for _ in range(lengthData)]
 
-# criando dados escalares
+#%% criando dados escalares
 varEscalares = ['heading', 'pitch', 'roll', 'temp', 'press']
 
 # limites de cada variável
@@ -48,7 +49,6 @@ for var in varEscalares:
         senoide = [math.sin(_) for _ in np.linspace(0,180,lengthData)]
         for index in range(len(senoide)):
             senoide[index] += filtro[index]*senoide[index]
-
         cell[var] = [_ + limites['press'] for _ in senoide]
         
     elif var == 'temp':
@@ -58,20 +58,82 @@ for var in varEscalares:
         
         for index in range(len(senoide)):
             senoide[index] += filtro[index]*senoide[index]
-            
         temp = [limites[var] + s for s in senoide]
         cell[var] = temp
     else:
-        cell[var] = [round(random.uniform(limites[var][0],limites[var][1]),3)\
+        data = [round(random.uniform(limites[var][0],limites[var][1]),3)\
                      for _ in range(lengthData)]
+        roundData = gaussian_filter1d(data, sigma = 10) 
+        cell[var] = roundData
+        
+# verificando visualmente os dados
+for var in varEscalares:
+    plt.figure()
+    plt.plot(cell[var], label = var)
+    plt.title('max %s %.2f'%(var, max(cell[var])))
 
-plt.plot([math.sin(_) for _ in values])
+# criando spikes
+dfout = {}
+# exportando variáveis criadas
+for var in varEscalares:
+    dfout[var] = pd.DataFrame(cell[var],dateTime)
+    dfout[var].to_csv('%s.csv'%var, header = None, float_format = '%.3f')
 
-# a = [math.sin(_) for _ in values]
-# b = [_/6 for _ in a]
+#%% criando dados matriciais
 
-# plt.plot(filtro)
-# plt.plot(senoide)
-# plt.grid()
+varMatricial = ['vel', 'dir', 'amp', 'cor']
 
-# plt.plot(temp)
+celulasTotais = int(max(cell['press'])) + 1
+
+fatorVel = np.linspace(.25, 1, celulasTotais)
+sigma = math.sqrt(0.15)
+x = np.linspace(0 - sigma, 0 + sigma, celulasTotais)
+fatorAmp = stats.norm.pdf(x, 0, sigma)*90
+min(fatorAmp)
+max(fatorAmp)
+
+shape = [len(dateTime), celulasTotais]
+cellMatriz = {}
+
+# criando matriz "vazia"
+array = np.empty(shape)
+
+limites = {'vel':[1,2],
+           'dir':[[30,120],[210,300],[120,210]],
+           'amp':[max(fatorAmp), 100],
+           'cor':[max(fatorAmp)*2, max(fatorAmp)*2.5]
+           }
+
+# criando array de dados dentro das celulas
+for var in varMatricial:
+    cellMatriz[var] = np.copy(array)
+    if var == 'vel':
+        for row in range(len(array)):
+            cellMatriz[var][row,:] = [random.uniform(limites[var][0],limites[var][1])\
+                                      for _ in range(celulasTotais)]
+            for col in range(celulasTotais):
+                cellMatriz[var][row,col] = fatorVel[col]*cellMatriz[var][row,col]
+            cellMatriz[var][row,round(cell['press'][row])] =\
+                cellMatriz[var][row,round(cell['press'][row])]*1.25
+    # # verificação visual do campo de velocidade
+    # plt.imshow(cellMatriz['vel'].T,
+    #            aspect='auto',
+    #            interpolation = 'nearest',
+    #            origin = 'lower',
+    #            cmap='jet')
+    
+    elif var == 'dir': # levar em consideração a maré para alternancia da direção
+        for row in range(len(array)):
+            cellMatriz[var][row,:] = [random.uniform(limites[var][0],limites[var][1])\
+                                      for _ in range(celulasTotais)]
+                
+            
+    elif var == 'amp':
+        for row in range(len(array)):
+            cellMatriz[var][row,:] = [random.uniform(limites[var],limites[var])\
+                                      for _ in range(celulasTotais)]
+    else:
+        for row in range(len(array)):
+            cellMatriz[var][row,:] = [random.uniform(limites[var],limites[var])\
+                                      for _ in range(celulasTotais)]
+        
--- tt.py
+++ tt.py
@@ -1,1 +1,6 @@
 import pandas as pd
+import numpy as np
+import windrose
+import scipy
+
+df = pd.DataFrame()
